4e0865234fb8bb9be339d2bb1736553018fecac6
MCV46316 <MCV46316@som-mcv46316.wlan.musc.edu>
2018-09-29 13:23:04 -0400

    This is test 1 of github

---
+++ JavaApplication3/src/medSimGUI/MedSimGUID.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package medSimGUI;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author MCV46316
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+
+    public static void main(final String[] args) {
+        final JFrame parent = new JFrame();
+        JButton button = new JButton();
+
+        button.setText("Click me to show dialog!");
+        parent.add(button);
+        parent.pack();
+        parent.setVisible(true);
+
+        button.addActionListener(new java.awt.event.ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                String name = JOptionPane.showInputDialog(parent,
+                        "What is your name?", null);
+            }
+        });
+    }
+}
+        
+        
+      
